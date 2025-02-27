f410a4a54a5b2f9ab48e12785fa20b1a615b9ea5
Benjamin <carlosbpac@gmail.com>
2025-02-27 17:49:54 -0600

    feat: Complejidad O

--- code_challenge_3/src/index.ts
+++ code_challenge_3/src/index.ts
@@ -1,3 +1,24 @@
+/***
+ * let find_key = null;
+        for (const [key, value] of this.students) {
+            if (value.sameLastname(student)) {
+                find_key = key;
+                break;
+            }
+        }
+        if (find_key) {
+            this.students.delete(find_key);
+        }
+
+        this.students.set(mark, student);
+
+    LA complejidad de este algoritmo es O(n) ya que recorre todos los elementos del mapa para encontrar si hay un estudiante con el mismo apellido.
+    Si encuentra un estudiante con el mismo apellido, lo elimina y agrega el nuevo estudiante.
+    Si no encuentra un estudiante con el mismo apellido, simplemente agrega el nuevo estudiante.
+ */
+
+
+
 import { Departament } from "./models/Departament";
 import { Student } from "./models/Student";
 
