f2a497ab48672b504d5c781831009d18fa83572d
benjamin <carlosbpac@gmail.com>
2025-03-06 17:28:57 -0600

    feat: create and list drivers

--- code_challenge_4/src/main/java/cresb/codeChallengue/controller/DriverController.java
+++ code_challenge_4/src/main/java/cresb/codeChallengue/controller/DriverController.java
@@ -1,32 +1,36 @@
 package cresb.codeChallengue.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import cresb.codeChallengue.dto.DriverDTO;
 import cresb.codeChallengue.model.Driver;
 import cresb.codeChallengue.service.DriverService;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/driver")
 public class DriverController {
-    
+
     @Autowired
     private DriverService driverService;
 
     @GetMapping("/all")
-    public String getAllDrivers() {
-        return driverService.getAllDrivers().toString();
+    public ResponseEntity<List<Driver>> getAllDrivers() {
+        List<Driver> drivers = driverService.getAllDrivers();
+        return ResponseEntity.ok(drivers);
     }
 
-    @GetMapping("/create")
-    public String createDriver(@RequestParam String name, @RequestParam int age) {
-        Driver driver = new Driver(name, age);
-        return driverService.createDriver(driver).toString();
+    @PostMapping("/create")
+    public ResponseEntity<Driver> createDriver(@RequestBody DriverDTO driverDTO) {
+        Driver driver = new Driver(driverDTO.getName(), driverDTO.getAge());
+        Driver createdDriver = driverService.createDriver(driver);
+        return ResponseEntity.ok(createdDriver);
     }
-    
-    
 }
