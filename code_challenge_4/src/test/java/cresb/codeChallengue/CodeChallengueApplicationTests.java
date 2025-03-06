a4aa30a358b372075ac93b8dc7e00ffdccfa93ac
benjamin <carlosbpac@gmail.com>
2025-03-06 16:55:58 -0600

    add: Proyect init

---
+++ code_challenge_4/src/test/java/cresb/codeChallengue/CodeChallengueApplicationTests.java
@@ -0,0 +1,13 @@
+package cresb.codeChallengue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeChallengueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
